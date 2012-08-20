00709ce378da06e3ee640956edf6c49c3fb83c4a
Greg Benison <gbenison@gmail.com>
2012-08-19 19:40:27 -0700

    more capistrano customization

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,11 +2,16 @@ set :application, "air-toxics"
 set :repository,  "git://github.com/gbenison/gwenomatic.git"
 set :deploy_to,   "/home/gbenison/air-toxics.com"
 
+# Reference: http://stackoverflow.com/questions/3023857/capistrano-and-deployment-of-a-website-from-github
+set :normalize_assert_timestamps, false
+
 set :scm, :git
 set :user, "gbenison"
 set :use_sudo, false
 
 role :web, "watanabe.dreamhost.com"
+role :app, "watanabe.dreamhost.com"
+role :db,  "watanabe.dreamhost.com"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
