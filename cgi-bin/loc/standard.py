00562f372de016d4bd24ebd2dd6552a6ad7015c7
Greg Benison <gbenison@gmail.com>
2012-12-20 22:51:47 -0800

    standards class capable of reading standards files

--- cgi-bin/loc/standard.py
+++ cgi-bin/loc/standard.py
@@ -12,15 +12,26 @@ class Standard():
         metafile.close()
         self.datafile=dir + '/data'
         self._cache = {}
-        self.csv_dialect = None
-        self.fh_data     = None
-        self.csv_reader  = None
+        self._csv_dialect = None
+        self._fh_data     = None
+        self._csv_reader  = None
 
     def lookup(self, cas):
-        # check cache
-        # open filehandle
-        # FIXME
-        pass
+        if not cas in self._cache:
+            self.prefetch([cas])
+        if cas in self._cache:
+            return self._cache[cas]
+        else:
+            return None
+
+    def prefetch(self, l_cas):
+        reader = self._f_csv_reader()
+        for row in reader:
+            if 'CAS' in row:
+                cas = row['CAS']
+                if cas in l_cas:
+                    self._cache[cas] = row
+        return self
 
     def _pull_from_cache(self, cas):
         if cas in self._cache:
@@ -28,15 +39,17 @@ class Standard():
         else:
             return None
 
-    def prefetch(self, l_cas):
-        if self.csv_reader is None:
-            assert (self.fh_data is None)
+    def _f_csv_reader(self):
+        if self._csv_reader is None:
+            assert (self._fh_data is None)
             fieldnames = map(lambda c: c['name'], self.meta['columns'])
-            self.fh_data=open(self.datafile, 'r')
-            if self.csv_dialect is None:
-                self.csv_dialect = csv.Sniffer().sniff(self.fh_data.read(1024))
-                self.fh_data.seek(0)
-            self.csv_reader = csv.DictReader(self.fh_data,
+            self._fh_data=open(self.datafile, 'r')
+            if self._csv_dialect is None:
+                self._csv_dialect = csv.Sniffer().sniff(self._fh_data.read(1024))
+                self._fh_data.seek(0)
+            self._csv_reader = csv.DictReader(self._fh_data,
                                              fieldnames,
-                                             dialect=self.csv_dialect)
+                                             dialect=self._csv_dialect)
+        self._fh_data.seek(0)
+        return self._csv_reader
 
