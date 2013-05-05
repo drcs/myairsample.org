c5bd30195ace32c0e27dc257bd8a550deea19c71
Greg Benison <gbenison@gmail.com>
2013-05-05 09:52:50 -0700

    change comparison description to two-and-a-half, etc.

--- loc_app/util.py
+++ loc_app/util.py
@@ -3,19 +3,35 @@ from math import log10
 def canonical_name(s):
     try: return s.lower().strip()
     except AttributeError: return s
+    
+def _first_decile_comparison_description(compare):
+    factor  = int(compare + 0.25)
+    decimal = (compare + 0.25) - factor
+    factor_str = {
+        1: 'one',
+        2: 'two',
+        3: 'three',
+        4: 'four',
+        5: 'five',
+        6: 'six',
+        7: 'seven',
+        8: 'eight',
+        9: 'nine'
+    }[factor]
+
+    if   (decimal < 0.5):  return factor_str + ' times'
+    return                        factor_str + '-and-a-half times'
 
 def describe_comparison(level,reference):
     """Describe the comparison between a level and a reference as an integer ratio,
        and one of Gwen's descriptive words"""
     compare = level / reference
-    decimal = compare - int(compare)
-    factor  = int(compare)
-    description = 'unknown'
-    if   (compare <= 1.0): return "is below"
-    elif (compare < 1.8):  return "exceeds"
-    elif (decimal < 0.2):  return "is about " + str(factor) + " times"
-    elif (decimal < 0.8):  return "is _over_ " + str(factor) + " times"
-    else:                  return "is _nearly_ " + str(factor + 1) + " times"
+
+    if compare > 10:       return "is " + int(round(compare)) + " times"
+    if compare > 1.25:     return "is " + _first_decile_comparison_description(compare)
+    if compare > 1.0:      return "exceeds"
+    
+    return "is below"
         
 def convert_units(level, from_units, to_units, mw=None):
     mw_air = 28.9666      # g mol-1
