2c261531cc9c342d0675b9202176437cd3204c23
Greg Benison <gbenison@gmail.com>
2013-07-07 09:56:26 -0700

    Chemical name validation: work with newly-added chemicals

--- loc_app/data/scripts/loc.js
+++ loc_app/data/scripts/loc.js
@@ -1,8 +1,22 @@
+function validateChemicalName() {
+    var elem = $(this)
+    $.get('api/validate/' + elem.val() , function(data) {
+	if (data == "NA") {
+	    if (elem.next(".warning").length == 0) {
+		elem.after($("<span class='warning'> !!!\"" + elem.val() + "\" is either misspelled, or not in the database !!!</span>"))
+	    }
+	} else {
+	    elem.next(".warning").remove()
+	}
+    });
+}
+
 $( document ).ready(function () {
     var chem_n = 4;
     $( "#add_chemicals_button" ).click(function (event) {
 	var newinput = $("<input />").attr("name","chem" + chem_n).attr("tabindex", chem_n*10)
 	$("#chemical_name_input").append(newinput)
+	newinput.focusout(validateChemicalName)
 	newinput.focus()
 	$("#chemical_level_input").append($("<input />").attr("name","report" + chem_n).attr("tabindex", chem_n*10 + 1))
 	chem_n = chem_n + 1;
@@ -15,16 +29,5 @@ $( document ).ready(function () {
 	event.preventDefault();
     });
 
-    $("#chemical_name_input input").focusout(function() {
-	var elem = $(this)
-	$.get('api/validate/' + elem.val() , function(data) {
-	    if (data == "NA") {
-		if (elem.next(".warning").length == 0) {
-		    elem.after($("<span class='warning'> !!!\"" + elem.val() + "\" is either misspelled, or not in the database !!!</span>"))
-		}
-	    } else {
-		elem.next(".warning").remove()
-	    }
-	});
-    });
+    $("#chemical_name_input input").focusout(validateChemicalName)
 });
