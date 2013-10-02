4606cc66970d625ca21819f98c64d3c73e431ea7
Greg Benison <gbenison@gmail.com>
2013-10-02 07:48:37 -0700

    Creates a TextOptions class and uses it to populate selection

---
+++ loc_app/options.py
@@ -0,0 +1,29 @@
+class TextOptions:
+
+  selection = []  
+
+  groups = [
+    ('both',
+     '''an explanation of both units and levels of concern''',
+     ['units','levels']),
+    ('units',
+     '''an explanation of units only''',
+     ['units']),
+    ('levels',
+     '''an explanation of levels of concern only''',
+     ['levels']),
+    ('none',
+     '''no text at the end of the report''',
+     []) ]
+
+  def select_group(self, group_name):
+      for (name,desc,selection) in self.groups:
+          if name == group_name:
+              self.selection = selection
+
+  def selected(self, item):
+      return item in self.selection
+
+  def group_names(self):
+      return map(lambda (k,d,x): (k, d), self.groups)
+
